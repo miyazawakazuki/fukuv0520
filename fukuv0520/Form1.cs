bc4461a5da23a76437567daf1b8cbe2309b23802
satthubongdem <49702341+satthubongdem@users.noreply.github.com>
2021-05-20 10:07:40 +0900

    Initial commit

---
+++ fukuv0520/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuv0520
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "押された";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Top = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+        }
+    }
+}
